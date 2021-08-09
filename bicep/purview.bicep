99cacd64cf5f9b732210df4b078e0e9ceb2a20c7
Taygan Rifat <contact@taygan.co>
2021-08-09 12:20:05 +0100

    init

---
+++ bicep/purview.bicep
@@ -0,0 +1,162 @@
+// az deployment group create -g "sandbox" -f ./bicep/bicep.bicep 
+param objectId string       // Azure AD (Current User)
+param clientId string       // CLIENT_ID
+param clientSecret string   // CLIENT_SECRET
+param spObjectId string     // OBJECT_ID
+param adminLogin string
+param suffix string = utcNow('ssfff')
+param timestamp string = utcNow()
+param roleNameGuid1 string = newGuid()
+param roleNameGuid2 string = newGuid()
+var location = resourceGroup().location
+var tenantId = subscription().tenantId
+@secure()
+param adminPassword string = newGuid()
+
+var roleDefinitionPrefix = '/subscriptions/${subscription().subscriptionId}/providers/Microsoft.Authorization/roleDefinitions'
+var role = {
+  PurviewDataCurator: '${roleDefinitionPrefix}/8a3c2885-9b38-4fd2-9d99-91af537c1347'
+  PurviewDataReader: '${roleDefinitionPrefix}/ff100721-1b9d-43d8-af52-42b69c1272db'
+  PurviewDataSourceAdministrator: '${roleDefinitionPrefix}/200bba9e-f0c8-430f-892b-6f0794863803'
+}
+
+// Azure Purview Account
+resource pv 'Microsoft.Purview/accounts@2020-12-01-preview' = {
+  name: 'pvdemo${suffix}-pv'
+  location: location
+  sku: {
+    name: 'Standard'
+    capacity: 4
+  }
+  identity: {
+    type: 'SystemAssigned'
+  }
+}
+
+// Assign Purview RBAC roles to Service Principal
+resource roleAssignment1 'Microsoft.Authorization/roleAssignments@2020-08-01-preview' = {
+  name: roleNameGuid1
+  scope: pv
+  properties: {
+    principalId: spObjectId
+    roleDefinitionId: role['PurviewDataCurator']
+    principalType: 'ServicePrincipal'
+  }
+}
+
+// Assign Purview RBAC roles to Service Principal
+resource roleAssignment2 'Microsoft.Authorization/roleAssignments@2020-08-01-preview' = {
+  name: roleNameGuid2
+  scope: pv
+  properties: {
+    principalId: spObjectId
+    roleDefinitionId: role['PurviewDataSourceAdministrator']
+    principalType: 'ServicePrincipal'
+  }
+}
+
+// Azure SQL Server
+resource sqlsvr 'Microsoft.Sql/servers@2021-02-01-preview' = {
+  name: 'pvdemo${suffix}-sqlsvr'
+  location: location
+  properties: {
+    administratorLogin: adminLogin
+    administratorLoginPassword: adminPassword
+  }
+  resource firewall1 'firewallRules' = {
+    name: 'allowAzure'
+    properties: {
+      startIpAddress: '0.0.0.0'
+      endIpAddress: '0.0.0.0'
+    }
+  }
+  resource firewall2 'firewallRules' = {
+    name: 'allowAll'
+    properties: {
+      startIpAddress: '0.0.0.0'
+      endIpAddress: '255.255.255.255'
+    }
+  }
+}
+
+// Azure SQL Database
+resource sqldb 'Microsoft.Sql/servers/databases@2021-02-01-preview' = {
+  parent: sqlsvr
+  name: 'pvdemo${suffix}-sqldb'
+  location: location
+  sku: {
+    name: 'GP_S_Gen5'
+    tier: 'GeneralPurpose'
+    family: 'Gen5'
+    capacity: 1
+  }
+  properties: {
+    collation: 'SQL_Latin1_General_CP1_CI_AS'
+    catalogCollation: 'SQL_Latin1_General_CP1_CI_AS'
+    autoPauseDelay: 60
+    requestedBackupStorageRedundancy: 'Local'
+    sampleName: 'AdventureWorksLT'
+  }
+}
+
+// Azure Key Vault
+resource kv 'Microsoft.KeyVault/vaults@2021-04-01-preview' = {
+  name: 'pvdemo${suffix}-keyvault'
+  location: location
+  properties: {
+    sku: {
+      family: 'A'
+      name: 'standard'
+    }
+    tenantId: tenantId
+    accessPolicies: [
+      {
+        tenantId: tenantId
+        objectId: objectId
+        permissions:{
+          secrets: [
+            'get'
+            'list'
+            'set'
+            'delete'
+            'recover'
+            'backup'
+            'restore'
+          ]
+        }
+      }
+      {
+        tenantId: tenantId
+        objectId: pv.identity.principalId
+        permissions: {
+          secrets: [
+            'get'
+            'list'
+          ]
+        }
+      }
+    ]
+  }
+  resource secret 'secrets' = {
+    name: 'sql-secret'
+    properties: {
+      value: adminPassword
+    }
+  }
+}
+
+// Data Plane Operations
+resource script 'Microsoft.Resources/deploymentScripts@2020-10-01' = {
+  name: 'script'
+  location: resourceGroup().location
+  kind: 'AzurePowerShell'
+  properties: {
+    azPowerShellVersion: '3.0'
+    arguments: '-tenant_id ${tenantId} -client_id ${clientId} -client_secret ${clientSecret} -purview_account ${pv.name} -vault_uri ${kv.properties.vaultUri}'
+    scriptContent: loadTextContent('purview.ps1')
+    forceUpdateTag: timestamp // script will run every time
+    retentionInterval: 'PT4H' // deploymentScript resource will delete itself in 4 hours
+  }
+}
+
+// output scriptOutput string = script.properties.outputs.putSource
---
+++ bicep/purview.ps1
@@ -0,0 +1,181 @@
+param([string]$tenant_id, [string]$client_id, [string]$client_secret, [string]$purview_account, [string]$vault_uri)
+
+# 1. Key Vault Connection
+# 2. Credential
+# 3. Source
+# 4. Scan
+# 5. Trigger (Run Scan)
+
+# Variables
+$scan_endpoint = "https://${purview_account}.scan.purview.azure.com"
+$proxy_endpoint = "https://${purview_account}.purview.azure.com/proxy"
+
+# [POST] Token
+function getToken([string]$tenant_id, [string]$client_id, [string]$client_secret) {
+    $uri = "https://login.windows.net/${tenant_id}/oauth2/token"
+    $body = @{
+        "grant_type" = "client_credentials"
+        "client_id" = $client_id
+        "client_secret" = $client_secret
+        "resource" = "https://purview.azure.net"
+    }
+    $params = @{
+        ContentType = "application/x-www-form-urlencoded"
+        Headers = @{"accept"="application/json"}
+        Body = $body
+        Method = "POST"
+        URI = $uri
+    }
+    
+    $token = Invoke-RestMethod @params
+    
+    Return "Bearer " + ($token.access_token).ToString()
+}
+
+# [PUT] Data Source
+function putSource([string]$token, [hashtable]$payload) {
+    $dataSourceName = $payload.name
+    $uri = "${scan_endpoint}/datasources/${dataSourceName}"
+    $params = @{
+        ContentType = "application/json"
+        Headers = @{"Authorization"=$token}
+        Body = ($payload | ConvertTo-Json)
+        Method = "PUT"
+        URI = $uri
+    }
+    $response = Invoke-RestMethod @params
+    Return $response
+}
+
+# [PUT] Key Vault
+function putVault([string]$token, [hashtable]$payload) {
+    $randomId = -join (((48..57)+(65..90)+(97..122)) * 80 |Get-Random -Count 3 |%{[char]$_})
+    $keyVaultName = "keyVault-${randomId}"
+    $uri = "${scan_endpoint}/azureKeyVaults/${keyVaultName}"
+    $params = @{
+        ContentType = "application/json"
+        Headers = @{"Authorization"=$token}
+        Body = ($payload | ConvertTo-Json)
+        Method = "PUT"
+        URI = $uri
+    }
+    $response = Invoke-RestMethod @params
+    Return $response
+}
+
+# [PUT] Credential
+function putCredential([string]$token, [hashtable]$payload) {
+    $credentialName = $payload.name
+    $uri = "${proxy_endpoint}/credentials/${credentialName}?api-version=2020-12-01-preview"
+    $params = @{
+        ContentType = "application/json"
+        Headers = @{"Authorization"=$token}
+        Body = ($payload | ConvertTo-Json -Depth 9)
+        Method = "PUT"
+        URI = $uri
+    }
+    $response = Invoke-RestMethod @params
+    Return $response
+}
+
+# [PUT] Scan
+function putScan([string]$token, [string]$dataSourceName, [hashtable]$payload) {
+    $scanName = $payload.name
+    $uri = "${scan_endpoint}/datasources/${dataSourceName}/scans/${scanName}"
+    $params = @{
+        ContentType = "application/json"
+        Headers = @{"Authorization"=$token}
+        Body = ($payload | ConvertTo-Json -Depth 9)
+        Method = "PUT"
+        URI = $uri
+    }
+    $response = Invoke-RestMethod @params
+    Return $response
+}
+
+# Run Scan
+function runScan([string]$dataSourceName, [string]$scanName) {
+    $guid = New-Guid
+    $runId = $guid.guid
+    $uri = "${scan_endpoint}/datasources/${dataSourceName}/scans/${scanName}/runs/${runId}?api-version=2018-12-01-preview"
+    $params = @{
+        Headers = @{"Authorization"=$token}
+        Method = "PUT"
+        URI = $uri
+    }
+    $response = Invoke-RestMethod @params
+    Return $response
+}
+
+# 1. Get Access Token
+$token = getToken $tenant_id $client_id $client_secret
+
+# 2. Create a Key Vault Connection
+$vault_payload = @{
+    properties = @{
+        baseUrl = $vault_uri
+        description = ""
+    }
+}
+$vault = putVault $token $vault_payload
+
+# 3. Create a Credential
+$credential_payload = @{
+    name = "sql-cred"
+    properties = @{
+        description = ""
+        type = "SqlAuth"
+        typeProperties = @{
+            password = @{
+                secretName = "sql-secret"
+                secretVersion = ""
+                store = @{
+                    referenceName = $vault.name
+                    type = "LinkedServiceReference"
+                }
+                type = "AzureKeyVaultSecret"
+            }
+            user = "sqladmin"
+        }
+    }
+    type = "Microsoft.Purview/accounts/credentials"
+}
+putCredential $token $credential_payload
+
+# 4. Create a Source
+$source_payload = @{
+    id = "datasources/AzureSqlDatabase"
+    kind = "AzureSqlDatabase"
+    name = "AzureSqlDatabase"
+    properties = @{
+        collection = ""
+        location = "uksouth"
+        parentCollection = ""
+        resourceGroup = "sandbox"
+        resourceName = "pvdemo04831-sqlsvr"
+        serverEndpoint = "pvdemo04831-sqlsvr.database.windows.net"
+        subscriptionId = "2c334b6c-e556-40ac-a4c0-c0d1d2e08ca0"
+    }
+}
+putSource $token $source_payload
+
+# 5. Create a Scan Configuration
+$randomId = -join (((48..57)+(65..90)+(97..122)) * 80 |Get-Random -Count 3 |%{[char]$_})
+$scanName = "Scan-${randomId}"
+$scan_payload = @{
+    kind = "AzureSqlDatabaseCredential"
+    name = $scanName
+    properties = @{
+        databaseName = "pvdemo04831-sqldb"
+        scanRulesetName = "AzureSqlDatabase"
+        scanRulesetType = "System"
+        serverEndpoint = "pvdemo04831-sqlsvr.database.windows.net"
+        credential = @{
+            credentialType = "SqlAuth"
+            referenceName = "sql-cred"
+        }
+    }
+}
+putScan $token "AzureSqlDatabase" $scan_payload
+
+# 6. Trigger Scan
