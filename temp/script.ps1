05db92243dc2e50cc1b03ded57a83fbc57817c06
Taygan Rifat <taygan@gmail.com>
2022-05-02 18:50:05 +0100

    script

--- temp/script.ps1
+++ temp/script.ps1
@@ -1,3 +1,4 @@
 Install-Module -Name Az.Purview -Force
+Import-Module -Name Az.Purview
 $kvConn = New-AzPurviewAzureKeyVaultObject -BaseUrl 'https://datascankv.vault.azure.net/' -Description 'This is a key vault'
 New-AzPurviewKeyVaultConnection -Endpoint 'https://pvdemo6uqbt-pv.purview.azure.com/' -KeyVaultName KeyVaultConnection2 -Body $kvConn
